a0660531695a85e396d5ec9866daf614ffc2458e
Kibahop <petri.lammi@puppeteers.net>
2023-03-24 00:21:56 +0200

    some rubocop
    Signed-off-by: Kibahop <petri.lammi@puppeteers.net>

--- lib/facter/wazuh.rb
+++ lib/facter/wazuh.rb
@@ -8,9 +8,9 @@ Facter.add(:wazuh) do
     @key_file = '/var/ossec/etc/client.keys'
     @state_file = '/var/ossec/var/run/wazuh-agentd.state'
     @ossec_file = '/var/ossec/etc/ossec.conf'
-    
+
     wazuh_hash = {}
-    
+
     def wazuh_agent_data(index)
       if File.exist?(@key_file)
         File.read(@key_file).split[index]
@@ -18,7 +18,7 @@ Facter.add(:wazuh) do
         ''
       end
     end
-    
+
     Facter.add('wazuh_agent_version') do
       setcode do
         cmd = case Facter.value('osfamily')
@@ -27,7 +27,7 @@ Facter.add(:wazuh) do
               when 'Debian'
                 '/usr/bin/dpkg-query -W -f="\\${Version}" wazuh-agent'
               end
-        stdout, stderr, status = Open3.capture3(cmd)
+        stdout, _stderr, status = Open3.capture3(cmd)
         if status.success?
           stdout.strip
         else
@@ -36,23 +36,12 @@ Facter.add(:wazuh) do
       end
     end
 
-    #def wazuh_agent_version
-    #  cmd = String.new
-    #  case Facter.value('osfamily')
-    #  when 'RedHat'
-    #    cmd = '/bin/rpm -q wazuh-agent --queryformat "%{VERSION}"'
-    #  when 'Debian'
-    #    cmd = '/usr/bin/dpkg-query -W -f="\\${Version}" wazuh-agent'
-    #  end
-    #  Facter::Core::Execution.execute(cmd)
-    #end
-    
     def get_ossec_conf_value(key)
       if File.exist?(@ossec_file)
         match = nil
         IO.foreach(@ossec_file) do |line|
-          if line.match(%r{^.*<#{key}>(.*)</#{key}>})
-            match = $1.strip
+          if line =~ %r{^.*<#{key}>(.*)</#{key}>}
+            match = Regexp.last_match(1).strip
             break
           end
         end
@@ -61,21 +50,11 @@ Facter.add(:wazuh) do
         ''
       end
     end
-    
-    #def get_ossec_conf_value(key)
-    #  if File.exist?(@ossec_file)
-    #    IO.readlines(@ossec_file).grep(%r{^.*<#{key}>}).map { |line|
-    #      line.match(%r{^.*<#{key}>(.*)</#{key}>}).captures.first.strip
-    #    }.compact.first
-    #  else
-    #    ''
-    #  end
-    #end
-    
+
     def wazuh_server_name
       get_ossec_conf_value('address') || ''
     end
-    
+
     if File.exist?(@state_file)
       File.foreach(@state_file) do |line|
         key, value = line.strip.split('=')
@@ -86,7 +65,7 @@ Facter.add(:wazuh) do
         end
       end
     end
-    
+
     wazuh_hash[:name] = wazuh_agent_data(1)
     wazuh_hash[:server] = wazuh_server_name
     wazuh_hash[:version] = wazuh_agent_version
