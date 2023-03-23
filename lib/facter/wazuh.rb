5e80ec15531ab6d5bb8d9af4f37ec4c7ff9e0b5b
Petri Lammi <petri.lammi@puppeteers.net>
2023-03-23 17:37:44 +0200

    Tune wazuh fact
    Signed-off-by: Petri Lammi <petri.lammi@puppeteers.net>

--- lib/facter/wazuh.rb
+++ lib/facter/wazuh.rb
@@ -8,9 +8,9 @@ Facter.add(:wazuh) do
     @key_file = '/var/ossec/etc/client.keys'
     @state_file = '/var/ossec/var/run/wazuh-agentd.state'
     @ossec_file = '/var/ossec/etc/ossec.conf'
-
+    
     wazuh_hash = {}
-
+    
     def wazuh_agent_data(index)
       if File.exist?(@key_file)
         File.read(@key_file).split[index]
@@ -18,7 +18,7 @@ Facter.add(:wazuh) do
         ''
       end
     end
-
+    
     def wazuh_agent_version
       cmd = String.new
       case Facter.value('osfamily')
@@ -29,32 +29,36 @@ Facter.add(:wazuh) do
       end
       Facter::Core::Execution.execute(cmd)
     end
-
+    
     def get_ossec_conf_value(key)
       if File.exist?(@ossec_file)
-        IO.readlines(@ossec_file).grep(%r{^.*<#{key}>}).map { |line|
-          line.match(%r{^.*<#{key}>(.*)</#{key}>}).captures.first.strip
-        }.compact.first && break
+        match = nil
+        IO.foreach(@ossec_file) do |line|
+          if line.match(%r{^.*<#{key}>(.*)</#{key}>})
+            match = $1.strip
+            break
+          end
+        end
+        match || ''
       else
         ''
       end
     end
-
+    
+    #def get_ossec_conf_value(key)
+    #  if File.exist?(@ossec_file)
+    #    IO.readlines(@ossec_file).grep(%r{^.*<#{key}>}).map { |line|
+    #      line.match(%r{^.*<#{key}>(.*)</#{key}>}).captures.first.strip
+    #    }.compact.first
+    #  else
+    #    ''
+    #  end
+    #end
+    
     def wazuh_server_name
       get_ossec_conf_value('address') || ''
     end
-
-    if File.exist?(@state_file)
-      File.foreach(@state_file) do |line|
-        key, value = line.strip.split('=')
-        case key
-        when 'status'
-          wazuh_hash['status'] = value.delete("'")
-          break
-        end
-      end
-    end
-
+    
     if File.exist?(@state_file)
       File.foreach(@state_file) do |line|
         key, value = line.strip.split('=')
@@ -65,7 +69,7 @@ Facter.add(:wazuh) do
         end
       end
     end
-
+    
     wazuh_hash[:name] = wazuh_agent_data(1)
     wazuh_hash[:server] = wazuh_server_name
     wazuh_hash[:version] = wazuh_agent_version
