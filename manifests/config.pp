fd87b5e451656ea88b2ff1365fac8ddaf9faa37f
Petri Lammi <petri.lammi@puppeteers.net>
2023-03-25 05:04:56 +0200

    Add dummy exec
    Signed-off-by: Petri Lammi <petri.lammi@puppeteers.net>

--- manifests/config.pp
+++ manifests/config.pp
@@ -23,19 +23,6 @@ class wazuh_agent::config {
     mode  => '0640',
   }
 
-  #$auth_command = Sensitive("/var/ossec/bin/agent-auth -A ${wazuh_agent::agent_name} -m ${wazuh_agent::server_name} -P ${wazuh_agent::password}")
-  $auth_command = ("/var/ossec/bin/agent-auth -A ${wazuh_agent::agent_name} -m ${wazuh_agent::server_name} -P ${wazuh_agent::password}")
-
-  exec { 'agent-auth-linux':
-    command   => $auth_command,
-    unless    => "/bin/egrep -q ${wazuh_agent::agent_name} ${keys_file}",
-    require   => [
-      File['ossec.conf'],
-      File[$keys_file],
-    ],
-    logoutput => true,
-  }
-
   $local_options_file = '/var/ossec/etc/local_internal_options.conf'
 
   $presence = $wazuh_agent::debug ? {
@@ -54,9 +41,22 @@ class wazuh_agent::config {
 
   if $facts.dig('wazuh', 'status') and $facts.dig('wazuh', 'status') != 'connected' {
 
-    notify { Class['wazuh_agent::service']:
-      message => "Wazuh: agent ${wazuh_agent::agent_name} is not connected",
-      require => Exec['agent-auth-linux'],
+    exec { 'notify':
+      cmd    => ':',
+      notify => Class['wazuh_agent::service'],
     }
+  }
+
+  #$auth_command = Sensitive("/var/ossec/bin/agent-auth -A ${wazuh_agent::agent_name} -m ${wazuh_agent::server_name} -P ${wazuh_agent::password}")
+  $auth_command = ("/var/ossec/bin/agent-auth -A ${wazuh_agent::agent_name} -m ${wazuh_agent::server_name} -P ${wazuh_agent::password}")
+
+  exec { 'agent-auth-linux':
+    command   => $auth_command,
+    unless    => "/bin/egrep -q ${wazuh_agent::agent_name} ${keys_file}",
+    require   => [
+      File['ossec.conf'],
+      File[$keys_file],
+    ],
+    logoutput => true,
   }
 }
