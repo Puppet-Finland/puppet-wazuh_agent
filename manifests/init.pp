65de6b7f56ea93918d1bf718d2da19794699dde3
Petri Lammi <petri.lammi@puppeteers.net>
2023-03-27 03:02:34 +0300

    Check api paramaters
    Signed-off-by: Petri Lammi <petri.lammi@puppeteers.net>

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,18 +26,38 @@ class wazuh_agent (
   Boolean $check_status                                     = true,
   Boolean $check_keepalive                                  = false,
   Boolean $check_last_ack                                   = false,
-  Boolean $check_state_match                                = false,
-  Boolean $check_name_match                                 = false,
+  Boolean $check_match_state                                = false,
+  Boolean $check_match_name                                 = false,
   Boolean $check_match_id                                   = false,
   Optional[Variant[Sensitive[String],String]] $api_username = undef,
   Optional[Variant[Sensitive[String],String]] $api_password = undef,
   Optional[String] $api_host                                = undef,
   Optional[Integer] $api_port                               = undef,
 ) {
+
+  # check api params
+  if $check_match_state or $check_match_name or  $check_match_id {
+    
+    $api_params = {
+      'param1' => $api_username,
+      'param2' => $api_password,
+      'param3' => $api_host,
+      'param4' => $api_port,
+    }
+    
+    $defined_params = $params.filter |$name, $value| {
+      $value != undef
+    }
+    
+    if ($defined_params.size > 0 and $defined_params.size < 4 and $defined_params.size != $params.size) {
+      fail('None or all of the api parameters must be set')
+    }
+  }
+
   contain 'wazuh_agent::install'
   contain 'wazuh_agent::config'
   contain 'wazuh_agent::service'
-
+  
   Class['wazuh_agent::install']
   -> Class['wazuh_agent::config']
   -> Class['wazuh_agent::service']
